f1b2ae3ff0a4ed867cf0928469ff782b5b3dce82
SSAXXARR <SSAXXARR@yandex.ru>
2021-12-01 09:08:45 +0500

    fix: fixed method answerT

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -3,6 +3,25 @@ import java.util.Scanner;
 public class User {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        while (true) {
+            answerT(scan);
+            String answer = scan.next();
+            if (answer.equals("нет") || answer.equals("Нет")) {
+                scan.close();
+                break;
+            } else if (answer.equals("да") || answer.equals("Да")) {
+
+            } else {
+                System.out.println("некоректный ответ");
+                scan.close();
+                break;
+            }
+
+        }
+    }
+
+
+    public static void answerT(Scanner scan){
         System.out.println("Введите имя нового контакта: ");
         String name = scan.next();
         System.out.println("Введите фимилию нового контакта: ");
@@ -15,11 +34,12 @@ public class User {
             PhoneBook book = new PhoneBook();
             book.addContact(name, surname, number);
             System.out.println("Данные успешно занесены в телефонную книгу");
+            System.out.println("Хотите занести еще один контакт? : ");
+
         }
         else{
             scan.close();
         }
-        scan.close();
     }
 
 
